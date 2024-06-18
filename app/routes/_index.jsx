3c464ef53a44c4b2ac5614c1ba7c5eb716da154e
FreeWiFi7749 <holofansaba@gmail.com>
2024-06-18 22:39:47 +0900

    fix

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,33 +1,34 @@
+import { Link } from '@remix-run/react';
 import ShareButtons from '../components/ShareButtons';
+import globalStyles from '../styles/global.css';
 import shareStyles from '../styles/share.css';
 import styleStyles from '../styles/style.css';
 
 export const links = () => [
+  { rel: "stylesheet", href: globalStyles },
   { rel: "stylesheet", href: shareStyles },
   { rel: "stylesheet", href: styleStyles }
 ];
 
-export const meta = () => [
-  {
-    charset: "utf-8",
-    title: "俺らゲイ",
-    viewport: "width=device-width,initial-scale=1",
-    "cache-control": "no-cache, no-store, must-revalidate",
-    "pragma": "no-cache",
-    "expires": "0",
-    "og:title": "俺らゲイ",
-    "og:type": "website",
-    "og:url": "https://we-are.gay",
-    "og:image": "https://media.discordapp.net/attachments/1092295569034203198/1220098537627324456/IMG_5898.png",
-    "og:description": "ゲイの心を大切に。",
-    "og:site_name": "俺らはゲイ。道を開けろ",
-    "twitter:card": "summary_large_image",
-    "twitter:site": "@freewifi_tech",
-    "twitter:title": "俺らゲイ",
-    "twitter:description": "ゲイの心を大切に",
-    "twitter:image": "https://media.discordapp.net/attachments/1092295569034203198/1220098537627324456/IMG_5898.png"
-  }
-];
+export const meta = () => ({
+  charset: "utf-8",
+  title: "alpha: 俺らゲイ",
+  viewport: "width=device-width,initial-scale=1",
+  "cache-control": "no-cache, no-store, must-revalidate",
+  "pragma": "no-cache",
+  "expires": "0",
+  "og:title": "俺らゲイ",
+  "og:type": "website",
+  "og:url": "https://alpha.we-are.gay",
+  "og:image": "https://media.discordapp.net/attachments/1092295569034203198/1220098537627324456/IMG_5898.png",
+  "og:description": "ゲイの心を大切に。",
+  "og:site_name": "俺らはゲイ。道を開けろ",
+  "twitter:card": "summary_large_image",
+  "twitter:site": "@freewifi_tech",
+  "twitter:title": "俺らゲイ",
+  "twitter:description": "ゲイの心を大切に",
+  "twitter:image": "https://media.discordapp.net/attachments/1092295569034203198/1220098537627324456/IMG_5898.png"
+});
 
 export default function Index() {
   return (
